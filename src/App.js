b72f258fd9ac3eb8a8774747493a1f158f822abf
mugane-wahome <muganewahome@gmail.com>
2024-06-10 14:57:03 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Advanced Calculator</h1>
+        <h1>Calc yangu</h1>
         <Calculator />
       </header>
     </div>
